531a7fbc939e446356f04109de835f5aa2c49fc4
OshriAmir94 <OshriAmir94@users.noreply.github.com>
2017-06-01 13:41:46 +0300

    Add files via upload

---
+++ src/Geometry.java
@@ -0,0 +1,29 @@
+/**
+ * Created by oshriamir on 5/31/17.
+ */
+public abstract class Geometry {
+    private Material material;
+    private int materialIndex;
+
+    public Geometry(Material material) {
+        this.material = material;
+    }
+    public Geometry(int _materialIndex) {
+        this.materialIndex = _materialIndex;
+    }
+    public Geometry(){}
+
+    public Material getMaterial() {
+        return material;
+    }
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public int materialIndex() {
+        return materialIndex;
+    }
+    public void setMaterialIndex(int materialIndex) {
+        this.materialIndex = materialIndex;
+    }
+}
